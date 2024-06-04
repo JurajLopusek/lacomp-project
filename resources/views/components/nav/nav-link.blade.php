9001cd653d7fadd866cb3f1a3c0e4b567c862ce0
juraj <jurajlopusek@gmail.com>
2024-06-04 19:29:45 +0200

    mensia uprava nav-baru 2

--- resources/views/components/nav/nav-link.blade.php
+++ resources/views/components/nav/nav-link.blade.php
@@ -1,7 +1,7 @@
 @props(['active' => false])
 
 <div class="flex justify-center">
-    <a class="{{ $active ? 'bg-red-500 text-white': 'text-white hover:border-2 border-red-500 hover:text-white'}} rounded-md px-10 py-2 mr-2 text-sm font-medium"
+    <a class="{{ $active ? 'bg-red-500 text-white': 'text-white hover:border-2 border-red-500 hover:text-white'}} rounded-md px-5 py-2 mr-2 text-sm font-medium"
        aria-current="{{ $active ? 'page': 'false' }}"
         {{ $attributes }}
     >{{ $slot }}</a>
