6ce746269bab64befc8c32c3ea477cab1c78fb32
juraj <jurajlopusek@gmail.com>
2024-06-11 19:23:51 +0200

    uprava div a nav

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -7,7 +7,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <!-- Styles -->
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -23,3 +23,7 @@
         </div>
     </body>
 </html>
+<script type="module">
+    import config from '../../../../tailwind.config.js';
+    console.log(config);
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
 
+    ],
+    theme: {
+        extend: {
+            boxShadow: {
+                'custom': '0 40px 8px rgba(0, 0, 0.1, 0.2)', // Definovanie vlastného tieňa
+            },
+            backgroundColor: {
+                'red-hover': '#f56565', // Definovanie vlastného pozadia
+            },
+            cursor: {
+                'pointer': 'pointer', // Definovanie vlastného kurzora
+            },
+        },
+    },
+    variants: {
+        extend: {
+            boxShadow: ['hover'], // Povolenie tieňa pri prechode kurzorom
+            backgroundColor: ['hover'], // Povolenie zmeny pozadia pri prechode kurzorom
+        },
+    },
+    plugins: [],
+}
