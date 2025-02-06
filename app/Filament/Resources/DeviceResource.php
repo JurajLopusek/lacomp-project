f23249b3a48adb09f316722a45593e3a2e9edba4
Martin Klacik <klacikmartin@protonmail.com>
2025-02-06 12:20:18 +0100

    feat: device resource

---
+++ app/Filament/Resources/DeviceResource.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Custom\Columns\IdColumnEnhanced;
+use App\Filament\Custom\Columns\TextColumnEnhanced;
+use App\Filament\Custom\Resource\ResourceEnhanced;
+use App\Filament\Enums\FilamentPanelNavigationGroupEnum;
+use App\Filament\Resources\DeviceResource\Pages;
+use App\Filament\Traits\CommonColumnsTrait;
+use App\Models\Device;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class DeviceResource extends ResourceEnhanced
+{
+    use CommonColumnsTrait;
+
+    protected static ?string $model = Device::class;
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $label = 'Zariadenie';
+    protected static ?string $pluralLabel = 'Zariadenia';
+    protected static ?string $navigationGroup = FilamentPanelNavigationGroupEnum::DEVICE->value;
+    protected static ?string $recordRouteKeyName = 'devices.id';
+
+    public static function table(Table $table): Table
+    {
+        $table
+            ->columns([
+                IdColumnEnhanced::factory(),
+                TextColumnEnhanced::make('serial_number'),
+                TextColumnEnhanced::make('name'),
+                TextColumnEnhanced::make('location'),
+                Tables\Columns\IconColumn::make('active'),
+            ])->defaultSort(self::$recordRouteKeyName, 'desc')
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+
+        return parent::table($table);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageDevices::route('/'),
+        ];
+    }
+}
