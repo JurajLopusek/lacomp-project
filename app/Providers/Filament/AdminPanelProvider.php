e43514f3a9f85e029cb73dedcf91bc50360ab6f0
Martin Klacik <klacikmartin@protonmail.com>
2025-02-16 18:07:31 +0100

    fix: remove dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -4,10 +4,8 @@ namespace App\Providers\Filament;
 
 use App\Filament\Enums\FilamentPanelEnum;
 use Exception;
-use Filament\Pages;
 use Filament\Panel;
 use Filament\Support\Colors\Color;
-use Filament\Widgets;
 
 class AdminPanelProvider extends BaseProvider
 {
@@ -30,14 +28,7 @@ class AdminPanelProvider extends BaseProvider
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
-            ->pages([
-                Pages\Dashboard::class,
-            ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
-            ]);
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets');
 
         return parent::panel($panel);
     }
