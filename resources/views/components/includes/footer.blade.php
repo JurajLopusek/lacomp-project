9408523e7feeba81a7aaa0a259ac1e14c89975b2
Juraj Lopusek <jurajlopusek@gmail.com>
2025-02-17 22:31:01 +0100

    footer.blade.php responsive

--- resources/views/components/includes/footer.blade.php
+++ resources/views/components/includes/footer.blade.php
@@ -1,63 +1,61 @@
 <footer class="bg-gray-700">
-    <div>
-        <div class="h-full flex justify-center mt-12">
-            <ul class="flex m-20 mr-10">
-                <li class="flex">
-                    <div class="flex h-10 w-10 items-center justify-center rounded bg-[#d60000] text-gray-50">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                             stroke-linejoin="round" class="h-6 w-6">
-                            <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
-                            <path
-                                d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z">
-                            </path>
-                        </svg>
-                    </div>
-                    <div class="ml-4 ">
-                        <h3 class="mb-2 text-lg font-medium leading-6 text-white">Adresa
-                        </h3>
-                        <p class="text-gray-600 dark:text-slate-400">La, s.r.o</p>
-                        <p class="text-gray-600 dark:text-slate-400">SNP 182</p>
-                        <p class="text-gray-600 dark:text-slate-400">Spišské Bystré</p>
-                        <p class="text-gray-600 dark:text-slate-400">05918</p>
-                    </div>
-                </li>
-                <li class="flex ml-40">
-                    <div class="flex h-10 w-10 items-center justify-center rounded bg-[#d60000] text-gray-50">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                             stroke-linejoin="round" class="h-6 w-6">
-                            <path
-                                d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2">
-                            </path>
-                            <path d="M15 7a2 2 0 0 1 2 2"></path>
-                            <path d="M15 3a6 6 0 0 1 6 6"></path>
-                        </svg>
-                    </div>
-                    <div class="ml-4">
-                        <h3 class="mb-2 text-lg font-medium leading-6 text-white">Kontakt
-                        </h3>
-                        <p class="text-gray-600 dark:text-slate-400">Telefón: +421 903 701 665</p>
-                        <p class="text-gray-600 dark:text-slate-400">E-mail: lacomp@lacomp.sk</p>
-                    </div>
-                </li>
-                <li class="flex ml-40">
-                    <div class="flex h-10 w-10 items-center justify-center rounded bg-[#d60000] text-gray-50">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                             stroke-linejoin="round" class="h-6 w-6">
-                            <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
-                            <path d="M12 7v5l3 3"></path>
-                        </svg>
-                    </div>
-                    <div class="ml-4 mb-4">
-                        <h3 class="mb-2 text-lg font-medium leading-6 text-white">Pracovná doba</h3>
-                        <p class="text-gray-600 dark:text-slate-400">Pondelok - Piatok: 07:00 - 17:00</p>
-                        <p class="text-gray-600 dark:text-slate-400">Sobota - Nedeľa: Zatvorené</p>
-                    </div>
-                </li>
-            </ul>
-        </div>
+    <div class="h-full flex justify-center mt-6 sm:mt-12">
+        <ul class="grid grid-cols-1 sm:grid-cols-3 sm:mx-10">
+            <li class="flex">
+                <div class="flex h-10 w-10 items-center justify-center rounded bg-[#d60000] text-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                         stroke-linejoin="round" class="h-6 w-6">
+                        <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+                        <path
+                            d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z">
+                        </path>
+                    </svg>
+                </div>
+                <div class="ml-4">
+                    <h3 class="mb-2 text-lg font-medium leading-6 text-white">Adresa
+                    </h3>
+                    <p class="text-gray-600 dark:text-slate-400">La, s.r.o</p>
+                    <p class="text-gray-600 dark:text-slate-400">SNP 182</p>
+                    <p class="text-gray-600 dark:text-slate-400">Spišské Bystré</p>
+                    <p class="text-gray-600 dark:text-slate-400">05918</p>
+                </div>
+            </li>
+            <li class="flex mt-6 sm:mt-0">
+                <div class="flex h-10 w-10 items-center justify-center rounded bg-[#d60000] text-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                         stroke-linejoin="round" class="h-6 w-6">
+                        <path
+                            d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2">
+                        </path>
+                        <path d="M15 7a2 2 0 0 1 2 2"></path>
+                        <path d="M15 3a6 6 0 0 1 6 6"></path>
+                    </svg>
+                </div>
+                <div class="ml-4">
+                    <h3 class="mb-2 text-lg font-medium leading-6 text-white">Kontakt
+                    </h3>
+                    <p class="text-gray-600 dark:text-slate-400">Telefón: +421 903 701 665</p>
+                    <p class="text-gray-600 dark:text-slate-400">E-mail: lacomp@lacomp.sk</p>
+                </div>
+            </li>
+            <li class="flex mt-6 sm:mt-0">
+                <div class="flex h-10 w-10 items-center justify-center rounded bg-[#d60000] text-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                         stroke-linejoin="round" class="h-6 w-6">
+                        <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
+                        <path d="M12 7v5l3 3"></path>
+                    </svg>
+                </div>
+                <div class="ml-4 mb-4">
+                    <h3 class="mb-2 text-lg font-medium leading-6 text-white">Pracovná doba</h3>
+                    <p class="text-gray-600 dark:text-slate-400">Pondelok - Piatok: 07:00 - 17:00</p>
+                    <p class="text-gray-600 dark:text-slate-400">Sobota - Nedeľa: Zatvorené</p>
+                </div>
+            </li>
+        </ul>
     </div>
     <div>
         <div class="w-full flex justify-center mb-2 text-lg font-medium leading-6 text-white">FOLLOW US:</div>
